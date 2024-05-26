770eaf6d45cf69fe52441906ca84f89e10651c6b
IvanOrsh <ksironov@mail.ru>
2024-05-26 20:02:00 +0300

    feat: implement pagination

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,8 +1,11 @@
+import { Suspense } from 'react';
+
+import { fetchInvoicesPages } from '@/lib/data';
 import { CreateInvoice } from '@/ui/invoices/buttons';
+import Pagination from '@/ui/invoices/pagination';
 import Table from '@/ui/invoices/table';
 import Search from '@/ui/search';
 import { InvoicesTableSkeleton } from '@/ui/skeletons';
-import { Suspense } from 'react';
 
 export default async function Page({
   searchParams,
@@ -14,6 +17,8 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -28,7 +33,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
